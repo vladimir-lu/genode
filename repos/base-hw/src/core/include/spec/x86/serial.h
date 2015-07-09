49ac3e304a6b73a09e628255929a63871f88eaad
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2015-07-09 11:45:05 +0200

    hw: use bios data area to detect com/uart ports
    Fixes #1625

--- repos/base-hw/src/core/include/spec/x86/serial.h
+++ repos/base-hw/src/core/include/spec/x86/serial.h
@@ -25,10 +25,38 @@ class Genode::Serial : public X86_uart_base
 {
 	private:
 
-		enum { IO_PORT = 0x3f8, CLOCK_UNUSED = 0 };
+		enum { CLOCK_UNUSED = 0 };
+
+		addr_t _port()
+		{
+			/**
+			 * Read BDA (Bios Data Area) to obtain I/O ports of COM
+			 * interfaces. The page must be mapped already,
+			 * see crt0_translation_table.s.
+			 */
+
+			enum {
+				MAP_ADDR_BDA         = 0x1ff000,
+
+				BDA_SERIAL_BASE_COM1 = 0x400,
+				BDA_EQUIPMENT_WORD   = 0x410,
+				BDA_EQUIPMENT_SERIAL_COUNT_MASK  = 0x7,
+				BDA_EQUIPMENT_SERIAL_COUNT_SHIFT = 9,
+			};
+
+			char * map_bda = reinterpret_cast<char *>(MAP_ADDR_BDA);
+			uint16_t serial_count = *reinterpret_cast<uint16_t *>(map_bda + BDA_EQUIPMENT_WORD);
+			serial_count >>= BDA_EQUIPMENT_SERIAL_COUNT_SHIFT;
+			serial_count &= BDA_EQUIPMENT_SERIAL_COUNT_MASK;
+
+			if (serial_count > 0)
+				return *reinterpret_cast<uint16_t *>(map_bda + BDA_SERIAL_BASE_COM1);
+
+			return 0;
+		}
 
 	public:
 
 		Serial(unsigned baud_rate)
-		: X86_uart_base(IO_PORT, CLOCK_UNUSED, baud_rate) { }
+		: X86_uart_base(_port(), CLOCK_UNUSED, baud_rate) { }
 };
